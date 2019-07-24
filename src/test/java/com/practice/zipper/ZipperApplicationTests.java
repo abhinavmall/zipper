2552f535c220910df1a32b00540f351706bc0028
Abhinav Mall <abhinav.0.mall@gmail.com>
2019-07-24 20:36:34 +0530

    First commit

---
+++ src/test/java/com/practice/zipper/ZipperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.practice.zipper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
